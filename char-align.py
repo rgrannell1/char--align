132f4fa2695692a9f57c0f6d913f907d0c6e5c21
rgrannell1 <r.grannell2@gmail.com>
2016-03-17 16:49:32 +0000

    more work on code align

--- char-align.py
+++ char-align.py
@@ -1,33 +1,67 @@
 #! /usr/bin/env python 3
 
-import os
 import sublime
 import sublime_plugin
-import random
-import re
-import sys
 
 
 
 
 
-__version__ = '0.1.0'
-__authors__ = ['Ryan Grannell (@RyanGrannell)']
+
+# dsa:              asddsaasdsadsad
+# dsaaaaaa:              asddsaasdsadsad
+# dfs:              asddsaasdsadsad
+# s:              asddsaasdsadsad
+# dfssssssssss:              asddsaasdsadsad
+# ds:              asddsaasdsadsad
+# dfskdfkdsfkdsfk:              asddsaasdsadsad
 
 
 
 
 
-class CharAlignCommand (sublime_plugin.TextCommand):
+def filterSelections (positions):
 
-	def run (self):
+	rows = { }
+
+	for row, col in positions:
+
+		if row in rows:
+			rows[row] = min(rows[row], col)
+		else:
+			rows[row] = col
+
+	return [(row, col) for row, col in rows.items( )]
+
+
+
+
+
+def alignSelections (self, edit):
+
+	view            = self.view
+	selections      = view.sel( )
+	cursorPositions = [ ]
+
+	for region in selections:
+		cursorPositions.append( view.rowcol(region.begin( )) )
+
+	toAlign = filterSelections(cursorPositions)
+	maxCol  = max(toAlign, key = lambda position: position[1])[1]
+
+	print( maxCol )
+
+
+
+class AlignCommand(sublime_plugin.TextCommand):
+
+	def run(self, edit):
 
 		view       = self.view
 		selection  = view.sel( )
-
 		settings   = view.settings( )
 		tab_size   = int(settings.get('tab_size', 8))
 		use_spaces = settings.get('translate_tabs_to_spaces')
 
-		if len(selection) > 1:
-			print(selection)
+		if len(selection) > 0:
+			alignSelections(self, edit)
